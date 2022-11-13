3e3f797f518063d01b7b812614d8f46a12cde96f
Ali Hassan Kazmi <sahkazmi100@gmail.com>
2022-11-13 19:44:19 +0500

    new ui

--- src/Pages/LoginPage/Login.tsx
+++ src/Pages/LoginPage/Login.tsx
@@ -1,35 +1,46 @@
-import React, { useState } from "react";
-import "./Login.css";
-import { useFormik } from "formik";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useFormik } from 'formik';
 import * as yup from "yup";
-import useLogin from "../../Hooks/useLogin";
-import { Link, useParams } from 'react-router-dom';
-import CircularProgress from '@mui/material/CircularProgress';
-// import CustomizedSnackbars from '../../../Components/Toast';
 
 
-const validationSchema = yup.object({
-  email: yup
-    .string()
-    .email("Enter a valid email")
-    .required("Email is required"),
-  password: yup
-    .string()
-    .min(6, "Password should be of minimum 6 characters length")
-    .required("Password is required"),
-});
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        HuddleBooth
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-const Login = () => {
-  // const [open] = React.useState(false);
+const theme = createTheme();
 
-  let { userType } = useParams();
-  console.log("usertype in login:", userType);
-  const [loading, setLoading] = useState(false);
+export default function SignUp() {
 
-  const { adminLogin } = useLogin(userType);
+  const validationSchema = yup.object({
+    email: yup
+      .string()
+      .email("Enter a valid email")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(6, "Password should be of minimum 6 characters length")
+      .required("Password is required"),
+  });
   const formik = useFormik({
     initialValues: {
       email: "",
@@ -37,49 +48,62 @@ const Login = () => {
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      adminLogin(values.email, values.password, setLoading);
-      setLoading(true);
+
     },
   });
 
-  // if (userType !== "customer" && userType !== "admin" && userType !== "brand") {
-  //   return <Navigate to="/404_Not_Found" />;
-  // }
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
 
   return (
-    <Box className="login">
-      <Box className="loginWrapper">
-        <Box className="loginLeft">
-          <h3 className="loginText">Social Media App</h3>
-          <Box className="loginDesc">Connect with each other!</Box>
-        </Box>
-        <Box className="loginRight">
-          <Box className="loginBox">
-            <Box
-              sx={{
-                "& .MuiTextField-root": { m: 1, width: "50ch" },
-              }}
-            >
-              <form onSubmit={formik.handleSubmit}>
-                {userType === "admin" ? (
-                  <h1 className="loginheading">Login as admin</h1>
-                ) : userType === "brand" ? (
-                  <h1 className="loginheading">Login as brand</h1>
-                ) : (
-                  <h1 className="loginheading">Login as customer</h1>
-                )}
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs" >
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            // bgcolor: '#f9c712'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: '#303030' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5" >
+            Sign up
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3, }}>
+            <Grid container spacing={2}>
+
+
+              <Grid item xs={12}>
                 <TextField
+                  required
                   fullWidth
-                  id="email"
+
+                  label="Email Address"
                   name="email"
-                  label="Email"
+                  autoComplete="email"
+                  id="email"
                   value={formik.values.email}
                   onChange={formik.handleChange}
                   error={formik.touched.email && Boolean(formik.errors.email)}
                   helperText={formik.touched.email && formik.errors.email}
                   onBlur={formik.handleBlur}
                 />
+              </Grid>
+              <Grid item xs={12}>
                 <TextField
+                  required
+                  fullWidth
                   id="password"
                   name="password"
                   label="Password"
@@ -92,72 +116,31 @@ const Login = () => {
                   helperText={formik.touched.password && formik.errors.password}
                   onBlur={formik.handleBlur}
                 />
-                {loading && (
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      marginLeft: "-6rem",
-                    }}
-                  >
-                    <CircularProgress></CircularProgress>
-                  </Box>
-                )}
+              </Grid>
+              <Grid item xs={12}>
 
-                <Button
-                  variant="contained"
-                  className="loginButton"
-                  type="submit"
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 2, mb: 5, backgroundColor: '#f9c712' }}
+            >
+              Sign Up
+            </Button>
 
-                  sx={{
-                    margin: "8px",
-                    color: "white",
-                    backgroundColor: "#303030",
-                  }}
-                >
-                  LOGIN
-                </Button>
-                <br></br>
-
-                <span id="span">Don't have an account?</span>
-                <br></br>
-                {userType === "admin" ? (
-                  <Link id="span2" to="/admin" >
-                    SIGNUP AS ADMIN!
-                  </Link>
-                ) : userType === "brand" ? (
-                  <Link id="span2" to="/brand">
-                    SIGNUP AS BRAND!
-                  </Link>
-                ) : (
-                  <Link id="span2" to="/customer">
-                    SIGNUP AS CUSTOMER!
-                  </Link>
-                )}
-                <br></br>
-                {userType === "admin" ? (
-                  <Link id="span2" to="/admin/forgotpassword">
-                    Forgot Password?
-                  </Link>
-                ) : userType === "brand" ? (
-                  <Link id="span2" to="/brand/forgotpassword">
-                    Forgot Password?
-                  </Link>
-                ) : (
-                  <Link id="span2" to="/customer/forgotpassword">
-                    Forgot Password?
-                  </Link>
-                )}
-                <br></br>
-                {/* <CustomizedSnackbars setOpen={open}></CustomizedSnackbars> */}
-              </form>
-            </Box>
+            <Grid container justifyContent="flex-end" sx={{ color: '#303030' }}>
+              <Grid item>
+                <Link href="#" variant="body2" sx={{ color: '#303030' }}>
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
           </Box>
         </Box>
-      </Box>
-    </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
   );
-};
-
-export default Login;
+}
